2c0717186cfbc3d7fbdeca4bee8b132fca516c29
Shadowstep1720 <74839190+Shadowstep1720@users.noreply.github.com>
2024-08-05 22:23:17 -0400

    Add event listener, add adjustable dimensions

--- scripts.js
+++ scripts.js
@@ -1,14 +1,31 @@
 const canvas = document.querySelector("#canvas");
-let gridSize = 16;
-for (let i = 0; i < gridSize; i++) {
-    const rowContainer = document.createElement("div");
-    rowContainer.setAttribute("class", "rowContainer");
-    canvas.appendChild(rowContainer);
+let gridWidth = 140;
+let gridHeight = 80;
+let canvasWidth = 1400;
+let canvasHeight = 800;
 
-    for (let y = 0; y < gridSize; y++) {
-        const pixel = document.createElement("div");
-        pixel.setAttribute("class", "pixel");
-        rowContainer.appendChild(pixel);
+canvas.style.width = `${canvasWidth}px`;
+canvas.style.height = `${canvasHeight}px`;
+
+//function createCanvas(gridSize) {
+    
+    for (let i = 0; i < gridHeight; i++) {
+        const rowContainer = document.createElement("div");
+        rowContainer.setAttribute("class", "rowContainer");
+        canvas.appendChild(rowContainer);
+    
+        for (let y = 0; y < gridWidth; y++) {
+            const pixel = document.createElement("div");
+            pixel.setAttribute("class", "blankPixel");
+            rowContainer.appendChild(pixel);
+
+            pixel.addEventListener("mouseover", colorPixel)
+        }
     }
-}
+//}
 
+function colorPixel(event) {
+    console.log("event handler called");
+    console.log(event.currentTarget);
+    event.currentTarget.setAttribute("class", "coloredPixel");
+}
