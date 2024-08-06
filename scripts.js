5e222b73d672b7cc5126c32cb689aaf8eb9ea81e
Shadowstep1720 <74839190+Shadowstep1720@users.noreply.github.com>
2024-08-05 21:12:49 -0400

    Create nested for loops to iterate through grid

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
-const container = document.querySelector("#container");
+const canvas = document.querySelector("#canvas");
 let gridSize = 16;
-//for (let i = 0; i < 16; )
+for (let i = 0; i < gridSize; i++) {
+    const rowContainer = document.createElement("div");
+    rowContainer.setAttribute("class", "rowContainer");
+    canvas.appendChild(rowContainer);
 
-const rowContainer = document.createElement("div");
-rowContainer.setAttribute("class", "rowContainer");
-container.appendChild(rowContainer);
+    for (let y = 0; y < gridSize; y++) {
+        const pixel = document.createElement("div");
+        pixel.setAttribute("class", "pixel");
+        rowContainer.appendChild(pixel);
+    }
+}
 
-const pixel = document.createElement("div");
-pixel.setAttribute("class", "pixel");
-rowContainer.appendChild(pixel);
