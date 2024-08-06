7b14b8a3878c8abde4cb8187db628bc29cdcc655
Shadowstep1720 <74839190+Shadowstep1720@users.noreply.github.com>
2024-08-05 23:39:18 -0400

    Add reset button and prompt to specify size

--- scripts.js
+++ scripts.js
@@ -1,22 +1,22 @@
 // TO DO: create input interface to let user set grid and canvas height
 
 const canvas = document.querySelector("#canvas");
-let gridWidth = 140;
-let gridHeight = 80;
-let canvasWidth = 1400;
-let canvasHeight = 800;
+let gridWidth = 100;
+let gridHeight = 100;
+let canvasWidth = 960;
+let canvasHeight = 960;
 
 canvas.style.width = `${canvasWidth}px`;
 canvas.style.height = `${canvasHeight}px`;
 
-//function createCanvas(gridSize) {
+function createCanvas(gridSize) {
     
-    for (let i = 0; i < gridHeight; i++) {
+    for (let i = 0; i < gridSize; i++) {
         const rowContainer = document.createElement("div");
         rowContainer.setAttribute("class", "rowContainer");
         canvas.appendChild(rowContainer);
     
-        for (let y = 0; y < gridWidth; y++) {
+        for (let y = 0; y < gridSize; y++) {
             const pixel = document.createElement("div");
             pixel.setAttribute("class", "blankPixel");
             rowContainer.appendChild(pixel);
@@ -24,10 +24,24 @@ canvas.style.height = `${canvasHeight}px`;
             pixel.addEventListener("mouseover", colorPixel)
         }
     }
-//}
+}
 
 function colorPixel(event) {
-    console.log("event handler called");
-    console.log(event.currentTarget);
     event.currentTarget.setAttribute("class", "coloredPixel");
 }
+
+function resetCanvas(gridSize) {
+    canvas.replaceChildren();
+    createCanvas(gridSize);
+}
+
+const resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener("click", () => {
+    let gridSize = parseInt(prompt("How many pixels would you like in the canvas? Please enter a number from 1-100."));
+    if (gridSize > 0 && gridSize < 101) {
+        resetCanvas(gridSize);
+    } else {
+        alert("Invalid entry. Please try again");
+        return
+    }
+})
